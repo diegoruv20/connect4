62f9b471b64e977f8ce164ef615ef77223ac6ba4
diegoruv20 <diegoruv0@gmail.com>
2020-09-27 13:11:38 -0700

    Arbitrary Board size and Cmd line args support

--- board.py
+++ board.py
@@ -1,13 +1,11 @@
 import numpy as np
 
-MAX_COL_COUNT = 7
-MAX_ROW_COUNT = 6
-
 class Connect4Board:
-    def __init__(self):
-        self._board = np.zeros((MAX_ROW_COUNT, MAX_COL_COUNT))
+    def __init__(self, numRows, numCols):
+        self._size = numRows, numCols
+        self._board = np.zeros((numRows, numCols))
         # Keeps track of open row index
-        self._validRows = [MAX_ROW_COUNT - 1 for i in range(MAX_COL_COUNT)]
+        self._validRows = [numRows - 1 for i in range(numCols)]
 
     def __repr__(self):
         return 'Board()'
@@ -15,8 +13,14 @@ class Connect4Board:
     def __str__(self):
         return str(self._board)
 
+    def numCols(self):
+        return self._size[1]
+    
+    def numRows(self):
+        return self._size[0]
+
     def isValidMove(self, col):
-        return col >= 0 and col < MAX_COL_COUNT - 1 and self._validRows[col] > - 1
+        return col >= 0 and col < self.numCols() and self._validRows[col] > - 1
 
     def placeToken(self, col, playerId):
         # Place in column
@@ -27,7 +31,7 @@ class Connect4Board:
         self._validRows[col] -= 1
 
     def _inBounds(self, row, col):
-        return row < MAX_ROW_COUNT and row > self._validRows[col] and col < MAX_COL_COUNT and col >= 0
+        return col < self.numCols() and col >= 0 and row < self.numRows() and row > self._validRows[col]
 
     def _countConsecutivePieces(self, rowStart, colStart, rowDelta=0, colDelta=0):
         playerId = self._board[rowStart, colStart]
@@ -44,7 +48,7 @@ class Connect4Board:
     def hasConnectFour(self):
         # iterate through last played piece in every col
         for col, row in enumerate(self._validRows):
-            if row == MAX_ROW_COUNT-1:
+            if row == self.numRows()-1:
                 continue
 
             row = row + 1
--- connect4.py
+++ connect4.py
@@ -1,10 +1,14 @@
-from board import Connect4Board, MAX_COL_COUNT
+from board import Connect4Board
 from random import randint
+import argparse
+
+DEFAULT_NUM_COL = 7
+DEFAULT_NUM_ROW = 6
 
 class Connect4():
-    def __init__(self):
+    def __init__(self, args):
         self._currPlayerId = 1
-        self._board = Connect4Board()
+        self._board = Connect4Board(args.numRows, args.numCols)
     
     def __str__(self):
         return str(self._board)
@@ -41,18 +45,16 @@ class Connect4():
         return self.getBoard().hasConnectFour()
 
     def getRandomAction(self):
-        return randint(0, MAX_COL_COUNT - 1)
+        return randint(0, self.getBoard().numCols() - 1)
 
-def main():
-    game = Connect4()
+def playGame(args):
+    game = Connect4(args)
     while True:
-        col = -1
-        try:
-            col = int(input(f"Enter col of move(1-{MAX_COL_COUNT}): "))
-            col -= 1
-        except ValueError:
-            print("Not a valid number...Try Again...")
+        col = input(f"Enter col of move(1-{args.numCols}): ")
+        if not isInt(col):
+            print("Not a valid int... Try Again...")
             continue
+        col = int(col) - 1
 
         if not game.getBoard().isValidMove(col):
             print("Invalid Move...")
@@ -66,5 +68,24 @@ def main():
         if game.isGameOver():
             print(f"GAME OVER! Player {currPlayer} Wins!")
             break
+
+def isInt(string):
+    try:
+        int(string)
+        return True
+    except:
+        return False
+    
+def main():
+    parser = argparse.ArgumentParser(description='Play a game of Connect 4')
+
+    parser.add_argument('--numRows', '-r',
+                        default=DEFAULT_NUM_ROW, type=int, help='Number of rows on the board')
+    parser.add_argument('--numCols', '-c',
+                        default=DEFAULT_NUM_COL, type=int, help='Number of columns on the board')
+
+    args = parser.parse_args()
+    playGame(args)
+    
 
 if __name__ == "__main__" : main()
